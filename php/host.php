a148d22db03ea51e5174ef2cbcd13ebaadf99fc5
Antaresque <1>
2017-11-09 10:25:19 +0100

    php files

---
+++ php/host.php
@@ -0,0 +1,7 @@
+<?php
+  $db_host = "localhost";
+  $db_login = "root";
+  $db_pass = "";
+  $db_name = "ng_test";
+
+  $connect = new mysqli($db_host, $db_login, $db_pass, $db_name);
---
+++ php/login.php
@@ -0,0 +1,20 @@
+<?php
+require_once('host.php');
+header('Access-Control-Allow-Origin: *'); //only for localhost
+header('Content-Type: application/json'); //json output
+
+$name = $_POST['name'];
+$pass = $_POST['pass'];
+
+$query = sprintf("SELECT * FROM users WHERE login = '$name' AND password = '$pass'");
+$result = $connect->query($query); 
+
+if($result->num_rows > 0) $login_result = true;
+else $login_result = false;
+
+$result->close();
+$connect->close();
+
+echo json_encode(array("login_result" => $login_result));
+
+
