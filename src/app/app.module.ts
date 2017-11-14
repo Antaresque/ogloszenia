97736d9f6d3064ba3918391faec1dc40b408a995
Antaresque <olekfer@yahoo.pl>
2017-11-14 13:40:54 +0100

    moduły

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//app-root
 import { AppComponent } from './app.component';
+
+//feature modules
+import { CoreModule } from './_core/core.module';
+
+//routing module
+
 import { LoginComponent } from './components/login/login.component';
-import { NaglowekComponent } from './components/naglowek/naglowek.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NaglowekComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule
