f08a319f2bd2192ca1a0a47ac032a3129f8a1dd8
Antaresque <olekfer@yahoo.pl>
2017-11-16 16:14:53 +0100

    add pagenotfound

---
+++ src/app/pagenotfound/pagenotfound.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './pagenotfound.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PageNotFoundComponent],
+  exports: [PageNotFoundComponent]
+})
+export class PageNotFoundModule { }
