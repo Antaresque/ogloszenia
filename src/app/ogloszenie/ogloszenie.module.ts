1eb4f186856696ea45852fbf8de3948afd48834e
Antaresque <olekfer@yahoo.pl>
2017-11-16 16:15:05 +0100

    add strona ogloszenia

---
+++ src/app/ogloszenie/ogloszenie.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OgloszenieComponent } from './ogloszenie.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OgloszenieComponent],
+  exports: [OgloszenieComponent]
+})
+export class OgloszenieModule { }
