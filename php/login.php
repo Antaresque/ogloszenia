a702bdaa8e6731c562eb6e11291862b09788e7bc
Antaresque <olekfer@yahoo.pl>
2017-11-18 15:02:31 +0100

    login.php require hash

--- php/login.php
+++ php/login.php
@@ -1,5 +1,6 @@
 <?php
 require_once('host.php');
+require_once('hash.php');
 header('Access-Control-Allow-Origin: *'); //only for localhost
 header('Content-Type: application/json'); //json output
 
@@ -7,7 +8,7 @@ $name = $_POST['name'];
 $pass = $_POST['pass'];
 
 $query = sprintf("SELECT * FROM users WHERE login = '$name' AND password = '$pass'");
-$result = $connect->query($query); 
+$result = $connect->query($query);
 
 if($result->num_rows > 0) $login_result = true;
 else $login_result = false;
