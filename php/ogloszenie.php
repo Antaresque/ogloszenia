6f9ba3ea0682e73ecc2e3c7924adda35659d2dfd
Antaresque <olekfer@yahoo.pl>
2017-11-18 21:07:56 +0100

    strona z ogloszeniem

--- php/ogloszenie.php
+++ php/ogloszenie.php
@@ -3,21 +3,20 @@ require_once('host.php');
 header('Access-Control-Allow-Origin: *'); //only for localhost
 header('Access-Control-Allow-Headers: Content-Type');//json output
 header('Content-Type: application/json'); //json output
-$connect = @new mysqli($host, $username, $password, $dbname);
 if($connect->connect_errno!=0)
 {
 	echo "Error: ".$connect->connect_errno;
 }
 else
 {
-	$id_og = $_POST['id_og'];
+  $array = json_decode(file_get_contents('php://input'), true);
+	$id_og = $array['id'];
 	$sql = sprintf("SELECT * FROM ogloszenia
 	WHERE id_og LIKE '$id_og'");
 	$result = $connect->query($sql);
-	while ($row = mysql_fetch_array($result, MYSQL_BOTH))
+	while ($row = mysqli_fetch_assoc($result))
 	{
-		$array = $row;
-		echo json_encode($array);
+		echo json_encode($row);
 	}
   $connect->close();
 }
--- src/app/ogloszenie/ogloszenie.component.ts
+++ src/app/ogloszenie/ogloszenie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-ogloszenie',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OgloszenieComponent implements OnInit {
 
-  constructor() { }
+  sub: any;
+  id: number;
+  dane: JSON;
+
+  constructor(private route: ActivatedRoute, private http: Http) { }
 
   ngOnInit() {
-  }
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    });
 
+    const data = {'id': this.id };
+    this.http.post('http://localhost/angular/php/ogloszenie.php', data).subscribe(
+      res => { this.dane = JSON.parse(res._body); }
+    );
+  }
 }
