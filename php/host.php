b0dd2fcbc721a7dd663a4e8b70b709b3811aa1e4
Antaresque <olekfer@yahoo.pl>
2017-11-18 14:10:48 +0100

    localhost db_name

--- php/host.php
+++ php/host.php
@@ -2,6 +2,6 @@
   $db_host = "localhost";
   $db_login = "root";
   $db_pass = "";
-  $db_name = "ng_test";
+  $db_name = "serwis";
 
   $connect = new mysqli($db_host, $db_login, $db_pass, $db_name);
