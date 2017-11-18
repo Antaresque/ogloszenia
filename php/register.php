21bdda94c525f109fae89e12a592a48b89c95db9
Antaresque <olekfer@yahoo.pl>
2017-11-18 15:02:58 +0100

    create register.php

---
+++ php/register.php
@@ -0,0 +1,8 @@
+<?php
+require_once('host.php');
+require_once('hash.php');
+header('Access-Control-Allow-Origin: *'); //only for localhost
+header('Content-Type: application/json'); //json output
+
+
+
