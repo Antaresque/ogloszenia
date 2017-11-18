42f988ee41188ac1696238db9a839f99fe463adc
Antaresque <olekfer@yahoo.pl>
2017-11-18 18:01:52 +0100

    add register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../_core/user/user.service';
+import { NgForm } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  wojewodztwa = [
+    'Region',
+    'woj. dolnośląskie',
+    'woj. kujawsko-pomorskie',
+    'woj. lubelskie',
+    'woj. lubuskie',
+    'woj. łódzkie',
+    'woj. małopolskie',
+    'woj. mazowieckie',
+    'woj. opolskie',
+    'woj. podkarpackie',
+    'woj. podlaskie',
+    'woj. pomorskie',
+    'woj. śląskie',
+    'woj. świętokrzyskie',
+    'woj. warmińsko-mazurskie',
+    'woj. wielkopolskie',
+    'woj. zachodniopomorskie'
+    ];
+
+  constructor(private user: UserService, private http: Http) { }
+
+  loading = false;
+  model: any = {};
 
   ngOnInit() {
   }
 
+  registerEvent() {
+    this.loading = true;
+    console.log(this.model);
+    this.http.post('http://localhost/angular/php/register.php', this.model).subscribe(
+        res => {
+            console.log(res);
+        },
+        error => {
+            this.loading = false;
+        });
+    }
+
+
+    /*this.http.post('http://localhost/angular/php/register.php', formData)
+        .subscribe(res => this.user.setUserLoggedIn(res.json().login_result)),
+                  (error) => console.log(error['_body']);
+    }
+    console.log('logged:', this.user.getUserLoggedIn());*/
 }
