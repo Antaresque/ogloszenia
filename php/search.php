e39a791e6e470de7f5e0511c59acb2d28cdf2383
Adam Bytniewski <adijedendwa@gmail.com>
2017-11-18 18:02:36 +0100

    search

---
+++ php/search.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "serwis";
+$connect = @new mysqli($host, $username, $password, $dbname);
+if($connect->connect_errno!=0)
+{
+	echo "Error: ".$connect->connect_errno;
+}
+else
+{
+	$search = $_POST['search'];
+	$kategoria = $_POST['kategoria'];
+	$sql = sprintf("SELECT id_og FROM ogloszenia 
+	INNER JOIN kategorie ON ogloszenia.id_kat = kategorie.id_kat
+	WHERE nazwa LIKE '*$search*' AND id_kat LIKE '$kategoria'");
+	$result = $connect->query($sql);
+	$connect->close();
+?>
