71d002a569ade2428f8a09ebe8e231f13e4b0ab7
Antaresque <olekfer@yahoo.pl>
2017-11-25 16:56:42 +0100

    gdy nie umiesz napisac autHorization

--- php/ogloszenie_kategorie.php
+++ php/ogloszenie_kategorie.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); //only for localhost
-header('Access-Control-Allow-Headers: Content-Type, Autorization');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json'); //json output
 
 require_once('_host.php');
--- php/user_register.php
+++ php/user_register.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); //only for localhost
-header('Access-Control-Allow-Headers: Content-Type, Autorization');//json output
+header('Access-Control-Allow-Headers: Content-Type, Authorization');//json output
 header('Content-Type: application/json'); //json output
 
 require_once('_host.php');
