f24711f05e3cccd28c54a5e0fc8b432e0285871c
Adam Bytniewski <adijedendwa@gmail.com>
2017-11-25 14:51:50 +0100

    module/wyszukiwarka

---
+++ src/app/wyszukiwarka/wyszukiwarka.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WyszukiwarkaComponent } from './wyszukiwarka.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WyszukiwarkaComponent]
+})
+export class WyszukiwarkaModule { }
