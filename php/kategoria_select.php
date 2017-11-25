4bca76f2557915216aa1a02093e47f29640f9af7
Adam Bytniewski <adijedendwa@gmail.com>
2017-11-25 18:42:47 +0100

    fix do kat php

--- php/kategoria_select.php
+++ php/kategoria_select.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json'); //json output
 require_once('_host.php');
 
 $array = json_decode(file_get_contents('php://input'), true);
-$result = DB::queryFirstRow("SELECT * FROM kategorie");
+$result = DB::query("SELECT * FROM kategorie WHERE id_kat_nad IS NULL");
 
 foreach($result as $row){
     array_push($array, $row['nazwa']);
