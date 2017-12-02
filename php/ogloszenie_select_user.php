61261d642b3039e15520a267f8d139447942066a
Antaresque <olekfer@yahoo.pl>
2017-12-02 18:29:03 +0100

    ogloszenie php

--- php/ogloszenie_select_user.php
+++ php/ogloszenie_select_user.php
@@ -4,34 +4,14 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json'); //json output
 
 require_once('_host.php');
-require_once('_JWT.php');
-use \Firebase\JWT\JWT;
 
 if(!empty(file_get_contents('php://input')))
-    {
-      $jwt = getBearerToken();
-      if(isset($jwt)){
-        try{
-          $payload = JWT::decode($jwt, $jwt_secret, array('HS256'));
-        }
-        catch(SignatureInvalidException $e){
-          http_response_code(401);
-        }
-        catch(UnexpectedValueException $e){
-          echo $e->getMessage();
-        }
+{
+  $array = json_decode(file_get_contents('php://input'), true);
+  $id = $array['id'];
 
-        $array = json_decode(file_get_contents('php://input'), true);
-        $id = $array['id'];
+  $result = DB::query("SELECT * FROM ogloszenia WHERE id_uz LIKE %i", $id);
 
-        if($payload->id == $id || $payload->funkcja == "admin")
-          $result = DB::query("SELECT * FROM ogloszenia WHERE id_uz LIKE %i", $id);
-        else http_response_code(401);
-
-        if(DB::count() == 0) $result = array('result' => false);
-        echo json_encode($result);
-      }
-      else {
-        http_response_code(401);
-      }
-    }
+  if(DB::count() == 0) $result = array('result' => false);
+  echo json_encode($result);
+}
