3d575723271e5b0d1d7612ea04c634f10c602b6e
Adam Bytniewski <adijedendwa@gmail.com>
2017-12-02 16:34:16 +0100

    adminuser

---
+++ src/app/admin/adminuser/adminuser.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-adminuser',
+  templateUrl: './adminuser.component.html',
+  styleUrls: ['./adminuser.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AdminuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
