c597353dab53a38703118bdd45f2a9967867a9b1
Antaresque <olekfer@yahoo.pl>
2017-12-03 22:19:15 +0100

    ogloszenie wiadomosc

--- src/app/panel/wiadomosci/wiadomosci-new/wiadomosci-new.component.ts
+++ src/app/panel/wiadomosci/wiadomosci-new/wiadomosci-new.component.ts
@@ -1,5 +1,5 @@
 import { WiadomosciService } from './../../../_core/wiadomosci/wiadomosci.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from './../../../_core/user/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
@@ -14,13 +14,20 @@ export class WiadomosciNewComponent implements OnInit {
   form: FormGroup;
   loading = false;
   wrongname = false;
+  model: any = {}
 
-  constructor(private fb: FormBuilder, private user: UserService, private router: Router, private wiad: WiadomosciService) { }
+  constructor(private fb: FormBuilder, private user: UserService, private router: Router, private wiad: WiadomosciService, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(
+      params => {
+        this.model['nazwa'] = params['nazwa'] || null;
+        this.model['odbiorca'] = params['odbiorca'] || null;
+      })
+    }
 
   ngOnInit() {
     this.form = this.fb.group({
-      temat: ['', Validators.required],
-      odbiorca: ['', Validators.required],
+      temat: [this.model['nazwa'], Validators.required],
+      odbiorca: [this.model['odbiorca'], Validators.required],
       tresc: ['', Validators.required],
     })
   }
@@ -31,17 +38,25 @@ export class WiadomosciNewComponent implements OnInit {
       this.user.search_name(this.form.value.odbiorca).subscribe(
         res => {
           let temp = JSON.parse(res['_body']);
-          this.form.value.odbiorca = temp.id_uz;
-          this.wiad.insert(this.form.value).subscribe(
-            res => {
-              console.log(res);
-              this.loading = false;
-              this.form.reset();
-              this.router.navigate(['/panel/wiadomosci']);
-            },
-            error => {
-              this.loading = false;
-            });
+          if(!(temp == null)){
+            this.form.value.odbiorca = temp.id_uz;
+
+            this.wiad.insert(this.form.value).subscribe(
+              res => {
+                console.log(res);
+                this.loading = false;
+                this.form.reset();
+                this.router.navigate(['/panel/wiadomosci']);
+              },
+              error => {
+                this.loading = false;
+                this.form.reset();
+              });
+            }
+          else {
+            this.loading = false;
+            this.form.hasError
+          }
         },
         error => {
           this.loading = false;
