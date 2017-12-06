553a82cf44ec2c972cd4797afa508c5ff46702d9
Antaresque <olekfer@yahoo.pl>
2017-12-06 19:28:11 +0100

    obserwowane_select

--- php/obserwowane_select.php
+++ php/obserwowane_select.php
@@ -1,5 +1,37 @@
 <?php
 require_once('_host.php');
 
-if(!empty(file_get_contents('php://input')))
-  {
+if(!empty(file_get_contents('php://input'))) {
+    $jwt = getBearerToken();
+    if(isset($jwt)){
+      try{
+        $payload = JWT::decode($jwt, $jwt_secret, array('HS256'));
+      }
+      catch(SignatureInvalidException $e){
+        http_response_code(401);
+      }
+      catch(UnexpectedValueException $e){
+        echo $e->getMessage();
+      }
+
+      $id = $payload->id;
+
+      $result = DB::queryFirstRow('SELECT obserwowane FROM uzytkownicy WHERE id_uz = %i', $id);
+      $obserwowane = json_decode($result['obserwowane']);
+      $data = array();
+
+      for($i = 0; $i < count($obserwowane); $i++){
+        $result = DB::query('SELECT o.*, k.nazwa as kategoria, u.login
+                             FROM ogloszenia as o
+                              INNER JOIN uzytkownicy as u ON o.id_uz = u.id_uz
+                              INNER JOIN kategorie as k ON o.id_kat = k.id_kat
+                             WHERE o.id_og = %i', $id);
+        array_push($data, $result);
+      }
+
+      echo json_encode($data);
+    }
+    else {
+      http_response_code(401);
+    }
+}
