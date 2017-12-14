41b21618ff9d653ccae5213e1dc096c87cb5cceb
Antaresque <olekfer@yahoo.pl>
2017-12-14 02:00:25 +0100

    ustawienia js

--- src/app/panel/ustawienia/ustawienia.component.ts
+++ src/app/panel/ustawienia/ustawienia.component.ts
@@ -1,7 +1,22 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from './../../_core/user/user.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Wojewodztwa } from '../../_core/ogloszenia/wojewodztwa.class';
+
+export function comparePassword(group: FormGroup) {
+  let passgroup = group.value;
+  return (passgroup.pass === passgroup.pass2) ? null : {
+    invalid: true
+  }
+}
+
+export function compareEmail(group: FormGroup) {
+  let passgroup = group.value;
+  return (passgroup.email === passgroup.email2) ? null : {
+    invalid: true
+  }
+}
 
 @Component({
   selector: 'app-ustawienia',
@@ -12,14 +27,24 @@ import { Wojewodztwa } from '../../_core/ogloszenia/wojewodztwa.class';
 export class UstawieniaComponent implements OnInit {
 
   loading = false;
+  loading2 = false;
+  loading3 = false;
+
   model: any = {};
-  model2: any = {};
-  model3: any = {};
+  form_mail: FormGroup;
+  form_pass: FormGroup;
+
+  success: any = [];
+  success2: any = [];
+  success3: any = [];
+
   errors: any = [];
+  errors2: any = [];
+  errors3: any = [];
 
   wojewodztwa = Wojewodztwa.wojewodztwa;
 
-  constructor(private user: UserService, private router: Router) { }
+  constructor(private fb: FormBuilder, private user: UserService, private router: Router) { }
 
   ngOnInit() {
     let payload = this.user.getPayload();
@@ -27,9 +52,76 @@ export class UstawieniaComponent implements OnInit {
       res => this.model = res,
       err => this.errors = err
     )
+
+    this.form_pass = this.fb.group({
+      passGroup: this.fb.group(
+        { pass: ['', [Validators.required] ],
+          pass2: ['', [Validators.required] ] },
+        { validator: comparePassword })});
+
+    this.form_mail = this.fb.group({
+      mailGroup: this.fb.group(
+        { email: ['', Validators.compose([Validators.required, Validators.email]) ],
+          email2: ['', Validators.compose([Validators.required, Validators.email])]},
+        { validator: compareEmail })});
   }
 
-  changeData(){}
-  changeMail(){}
-  changePassword(){}
+  changeData(){
+    this.loading = true;
+    this.user.changeData(this.model).subscribe(
+      res => {
+        this.success = res;
+        this.errors = [];
+        this.loading = false;
+        },
+      err => {
+        this.success = [];
+        this.errors = err;
+        this.loading = false;
+      });
+  }
+
+  changeMail(){
+    if(this.form_mail.valid){
+      this.loading2 = true;
+
+      const formValue = Object.assign({}, this.form_mail.value);
+      formValue.email = formValue.mailGroup.email;
+      delete formValue.mailGroup;
+
+      this.user.changeEmail(formValue).subscribe(
+        res => {
+          this.success2 = res;
+          this.errors2 = [];
+          this.loading = false;
+          },
+        err => {
+          this.success2 = [];
+          this.errors2 = err;
+          this.loading = false;
+        });
+    }
+  }
+
+  changePassword(){
+    if(this.form_pass.valid){
+      this.loading3 = true;
+
+      const formValue = Object.assign({}, this.form_mail.value);
+      formValue.pass = formValue.passGroup.pass;
+      delete formValue.passGroup;
+
+      this.user.changePass(formValue).subscribe(
+        res => {
+          this.success3 = res;
+          this.errors3 = [];
+          this.loading = false;
+          },
+        err => {
+          this.success3 = [];
+          this.errors3 = err;
+          this.loading = false;
+        });
+    }
+  }
 }
