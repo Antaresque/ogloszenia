77b2a0d85b66f530a3b3acf00dcc1a63b999072f
Antaresque <olekfer@yahoo.pl>
2017-12-14 02:30:13 +0100

    brawo ja x3

--- php/user/changepass.php
+++ php/user/changepass.php
@@ -5,11 +5,11 @@ $id = getPayload()->id;
 $haslo = $input['pass'];
 $data = DB::queryFirstRow('SELECT * FROM uzytkownicy WHERE id_uz = %i', $id);
 
-if($data['haslo'] == $haslo) error_message('UPDATE_PASS_IS_SAME');
+if(validate_pw($haslo, $data['haslo'])) error_message('UPDATE_PASS_IS_SAME');
 else {
 
   DB::update('uzytkownicy', array(
-          'haslo' => $haslo),
+          'haslo' => generate_hash($haslo)),
           'id_uz=%s', $id);
 
   if(DB::affectedRows() == 0) error_message('UPDATE_FAIL');
