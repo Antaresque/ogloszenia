f335aed17f82eb1250a90e4cfed3358ea98b64e8
Antaresque <olekfer@yahoo.pl>
2017-12-14 06:43:38 +0100

    markdown

--- src/app/_shared/shared.module.ts
+++ src/app/_shared/shared.module.ts
@@ -5,13 +5,16 @@ import { CommonModule } from '@angular/common';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { TimeAgoPipe } from './time-ago-pipe';
 import { TruncatePipe } from './truncate-pipe';
+import {MarkdownToHtmlModule} from 'markdown-to-html-pipe';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     RouterModule,
     ReactiveFormsModule,
-    NgxGalleryModule
+    NgxGalleryModule,
+    MarkdownToHtmlModule
   ],
   declarations: [TimeAgoPipe, TruncatePipe],
   exports: [
@@ -21,7 +24,8 @@ import { TruncatePipe } from './truncate-pipe';
     ReactiveFormsModule,
     NgxGalleryModule,
     TimeAgoPipe,
-    TruncatePipe
+    TruncatePipe,
+    MarkdownToHtmlModule
   ]
 })
 export class SharedModule { }
